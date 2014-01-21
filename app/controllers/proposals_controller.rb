961439d313e8f673d9ab1633df651f52eb2c56ae
Joren De Groof <git@jorendegroof.be>
2014-01-21 09:47:16 +0100

    Adds more layout to it all

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -2,4 +2,30 @@ class ProposalsController < ApplicationController
   def new
     @proposal = Proposal.new
   end
+
+  def create
+    @proposal = Proposal.new proposal_params
+    if @proposal.save
+      redirect_to :root, notice: 'Arrrr ye matey. Your captain thanks ye!'
+    else
+      flash.now[:error] = 'Uh Ow, please fill in everything that we need'
+      render :new
+    end
+  end
+
+private
+  def proposal_params
+    params.require(:proposal).permit(
+      :name,
+      :email,
+      :title,
+      :description,
+      :tags,
+      :bio,
+      :github,
+      :twitter,
+      :recidence,
+      :motivation,
+    )
+  end
 end
