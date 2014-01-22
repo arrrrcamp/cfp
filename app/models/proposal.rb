11c42e0c7b47c393a51a31b1ac9b8ddd9d336df0
Joren De Groof <git@jorendegroof.be>
2014-01-22 10:24:25 +0100

    Scramble personal fields

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -1,3 +1,12 @@
 class Proposal < ActiveRecord::Base
   validates_presence_of :title, :email, :name, :description, :bio, :recidence, :motivation
+
+  before_save :scramble
+
+private
+  def scramble
+    %w(name email bio github twitter recidence).each do |attr|
+      self.send "#{attr}=", Base64::encode64(self.send(attr).to_s)
+    end
+  end
 end
