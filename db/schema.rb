e72bdf681b819855b115a7d72f4702736d82b0b6
Joren De Groof <git@jorendegroof.be>
2014-03-27 15:29:23 +0100

    Adds user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140122134741) do
+ActiveRecord::Schema.define(version: 20140327142720) do
 
   create_table "proposals", force: true do |t|
     t.string   "name"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20140122134741) do
     t.datetime "updated_at"
     t.string   "identifier",  null: false
     t.string   "avatar"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
