caa664ff0f9158cb2549ed7cb6e4d65fb3e92ace
Joren De Groof <git@jorendegroof.be>
2014-03-27 15:42:57 +0100

    Adds sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Cfp::Application.routes.draw do
   root 'proposals#new'
   resources :proposals, only: [:new, :create, :edit, :update]
+  resources :sessions, only: [:new, :create, :destroy]
+  namespace :crew do
+    resources :proposals
+    root to: 'proposals#index'
+  end
+  get "log_out" => "sessions#destroy", :as => "log_out"
 end
