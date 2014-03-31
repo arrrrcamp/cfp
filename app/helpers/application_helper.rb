cebc0690ddfe20fab083a5afaad9460d22ca9875
Hannes Fostie <hannes@openminds.be>
2014-03-31 11:43:35 +0200

    Adds markdown helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def markdown_renderer
+    @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+  end
+
+  def markdown text
+    markdown_renderer.render(text).html_safe
+  end
 end
