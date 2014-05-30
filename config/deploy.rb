f4c326412fdeb3fdccff39767a7c6520c9714898
Joren De Groof <git@jorendegroof.be>
2014-05-30 11:36:49 +0200

    Adds uploads symlink task to deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,11 @@ server 'pro-009.openminds.be', :app, :web, :db, primary: true
 set :user, 'arrrrcamp2014'
 
 set :configs, %w[database]
+
+after 'deploy:update_code', 'symlinks:create'
+namespace :symlinks do
+  desc 'Create custom symlinks to shared and system folders'
+  task :create do
+    run "ln -nfs #{shared_path}/uploads #{release_path}/public/uploads"
+  end
+end
